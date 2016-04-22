f87cda0e3b597a4b5a6be7977e338c68d4aceb9d
Sergey Shambir <sergey.shambir.auto@gmail.com>
2016-04-22 10:25:36 +0300

    Добавил пример работы с кодировками
    Также поправил stdafx у примера разбора выражений

---
+++ strings/locales_and_encoding/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#include <SDKDDKVer.h>
+#include <tchar.h>
+#endif
+
+#include <cstdio>
+#include <clocale>
+#include <cctype>
+
+#include <locale>
+#include <codecvt>
+
